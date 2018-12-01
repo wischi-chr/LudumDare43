a178b10396526650bce7ea61784cfcf24b412284
marnaish <aberl.martin@gmail.com>
2018-12-01 19:08:14 +0100

    Add Player Movement

---
+++ src/unity/LudumDare43/Assets/CameraFollowObject.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowObject : MonoBehaviour
+{
+
+    public GameObject ObjectToFollow;
+    public bool LockX, LockY;
+    public bool DebugInformation = true;
+    public Vector3 DestinationDifference = Vector3.zero;
+    public float Offset = 2f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var destination = new Vector3(LockX ? 0 : ObjectToFollow.transform.position.x, LockY ? 0 : ObjectToFollow.transform.position.y, transform.position.z);
+        destination += DestinationDifference;
+        float distance = Vector3.Distance(transform.position, destination);
+
+        if (distance > Offset)
+        {
+
+            distance -= Offset;
+
+            var heading = destination - transform.position;
+            heading *= Time.deltaTime;
+            heading *= distance * 2;
+
+            transform.position = new Vector3(
+                LockX ? transform.position.x : transform.position.x + heading.x,
+                LockY ? transform.position.y : transform.position.y + heading.y,
+                transform.position.z);
+        }
+
+        if (DebugInformation)
+        {
+            DrawDebugInformation();
+        }
+    }
+
+    private void DrawDebugInformation()
+    {
+        var position = new Vector3(transform.position.x, transform.position.y, 0);
+        var destination = new Vector3(LockX ? 0 : ObjectToFollow.transform.position.x, LockY ? 0 : ObjectToFollow.transform.position.y, 0);
+        destination += DestinationDifference;
+
+        Debug.DrawRay(position, destination - position, Color.black);
+    }
+}
---
+++ src/unity/LudumDare43/Assets/PlayerMovement.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D compRigidBody;
+    private float distToGround;
+
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public float movementSpeed = 100f;
+    private float sprintMultiplyer = 2;
+    public float JumpHeight = 250;
+    public bool DebugInformation = true;
+    public int PossibleJumps = 1;
+    public int ExecutedJumps = 0;
+    public float JumpRayCast = 0.75f;
+
+    public LayerMask GroundLayer;
+
+
+    private bool jumping = false;
+    private Vector2 lookDirection = Vector2.right;
+
+    // Use this for initialization
+    void Start()
+    {
+        compRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float sprint = 1;
+
+        if (Input.GetKey(sprintKey))
+            sprint = sprintMultiplyer;
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f * sprint;
+
+        lookDirection = x > 0 ? Vector2.right : Vector2.left;
+
+
+        var grounded = IsGrounded();
+
+        if (Input.GetKeyDown(jumpKey) && grounded && !jumping)
+        {
+            Jump();
+            jumping = true;
+            ExecutedJumps = 1;
+        }
+        else if (!jumping && Input.GetKeyDown(jumpKey) && ExecutedJumps < PossibleJumps)
+        {
+            Jump();
+            ExecutedJumps++;
+        }
+
+        if (!Input.GetKey(jumpKey))
+        {
+            jumping = false;
+        }
+
+        //velocity method
+        var vel = (transform.right * x) * movementSpeed;
+        vel.y = compRigidBody.velocity.y;
+        compRigidBody.velocity = vel;
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+
+        if (DebugInformation)
+        {
+            DrawDebugInformation();
+        }
+    }
+
+    private void Jump()
+    {
+        compRigidBody.AddForce(Vector2.up * JumpHeight);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, JumpRayCast, GroundLayer).collider != null
+            || Physics2D.Raycast(transform.position - new Vector3(0.5f, 0, 0), Vector2.down, JumpRayCast, GroundLayer).collider != null
+            || Physics2D.Raycast(transform.position + new Vector3(0.5f, 0, 0), Vector2.down, JumpRayCast, GroundLayer).collider != null;
+    }
+
+    private void DrawDebugInformation()
+    {
+        Debug.DrawRay(transform.position, Vector2.down * JumpRayCast, Color.green);
+        Debug.DrawRay(transform.position - new Vector3(0.5f, 0, 0), Vector2.down * JumpRayCast, Color.green);
+        Debug.DrawRay(transform.position + new Vector3(0.5f, 0, 0), Vector2.down * JumpRayCast, Color.green);
+
+        Debug.DrawRay(transform.position, lookDirection * 2, Color.blue);
+    }
+}
