950f411d04b2ba4319af02be15c1a4f9267c9eb1
marnaish <aberl.martin@gmail.com>
2018-12-01 18:22:25 +0100

    Add Player sprite

--- src/unity/LudumDare43/Assets/HuskyTestScript.cs
+++ src/unity/LudumDare43/Assets/HuskyTestScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class HuskyTestScript : MonoBehaviour {
     public int mutliSpeed = 40;
     public Animator animator;
+    public SpriteRenderer SpriteRenderer;
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +16,16 @@ public class HuskyTestScript : MonoBehaviour {
 	void Update () {
         var speed = Input.GetAxisRaw("Horizontal") * mutliSpeed;
 
-        animator.SetFloat("Speed", speed);
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+
+
+        if (speed < -0.01 && !SpriteRenderer.flipX)
+        {
+            SpriteRenderer.flipX = true;
+        }
+        else if (speed > 0.01 && SpriteRenderer.flipX)
+        {
+            SpriteRenderer.flipX = false;
+        }
 	}
 }
