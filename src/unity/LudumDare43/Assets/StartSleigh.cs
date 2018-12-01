83e7d4920027b91ced70b24ff492d9008824ba45
marnaish <aberl.martin@gmail.com>
2018-12-01 20:30:25 +0100

    Add sleigh to main scene

---
+++ src/unity/LudumDare43/Assets/StartSleigh.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSleigh : MonoBehaviour {
+    public LayerMask SleighLayer;
+    public float JumpRayCast = 0.75f;
+    public GameObject Sleigh;
+    private Rigidbody2D sleighRigidBody;
+    public int Speed = 200;
+
+    // Use this for initialization
+    void Start () {
+        sleighRigidBody = Sleigh.GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var onSleigh = IsOnSleigh();
+
+        if(onSleigh)
+        {
+            // TODO: Move Sleigh
+        }
+    }
+
+
+    private bool IsOnSleigh()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, JumpRayCast, SleighLayer).collider != null
+            || Physics2D.Raycast(transform.position - new Vector3(0.5f, 0, 0), Vector2.down, JumpRayCast, SleighLayer).collider != null
+            || Physics2D.Raycast(transform.position + new Vector3(0.5f, 0, 0), Vector2.down, JumpRayCast, SleighLayer).collider != null;
+    }
+}
