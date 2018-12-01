b2ba756957dcd3d3c132c327df8698256b40617f
marnaish <aberl.martin@gmail.com>
2018-12-01 17:47:26 +0100

    Add husky sprite animation test scene

---
+++ src/unity/LudumDare43/Assets/HuskyTestScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HuskyTestScript : MonoBehaviour {
+    public int mutliSpeed = 40;
+    public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var speed = Input.GetAxisRaw("Horizontal") * mutliSpeed;
+
+        animator.SetFloat("Speed", speed);
+	}
+}
