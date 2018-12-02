6c6006af626fd460e975f193ea1ddd636ca65623
Christian Wischenbart <c.wischenbart@bitoxide.com>
2018-12-02 11:04:20 +0100

    Add distance counter.

---
+++ src/unity/LudumDare43/Assets/HudDistanceDisplay.cs
@@ -0,0 +1,23 @@
+﻿using Assets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudDistanceDisplay : MonoBehaviour {
+
+    Text hudDistanceText;
+
+    // Use this for initialization
+    void Start()
+    {
+        hudDistanceText = GameObject.Find("hud-distance-text").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hudDistanceText.text = string.Format("{0:# ##0.0}", GlobalGameState.Distance);
+        Debug.Log("Update Hud");
+    }
+}
